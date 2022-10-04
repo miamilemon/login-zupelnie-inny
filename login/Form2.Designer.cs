8b290a6b5acf2befebcebae93b6ec4b168adb7eb
krisu12345 <krzysiu.offical@wp.pl>
2022-10-04 13:24:36 +0200

    comm

--- login/Form2.Designer.cs
+++ login/Form2.Designer.cs
@@ -211,6 +211,7 @@
             this.button1.TabIndex = 6;
             this.button1.Text = "Dodaj";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form2
             // 
--- login/Form2.cs
+++ login/Form2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,18 @@ namespace login
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            // zapisywanie do pliku
+            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(docPath, "uczen.txt"),true))
+            {
+                outputFile.WriteLine(textBox2.Text+" "+textBox3.Text+" "+textBox4.Text+'\n');
+                SaveFileDialog theDialog = new SaveFileDialog();
+            }
         }
     }
 }
